b883d07f59d68fb0bfe7e4ef053801b677c1a14e
Michael Sober <michael.sober@tuhh.de>
2022-02-07 15:09:43 +0100

    Compute hash of commitments

--- poly_eval/scripts/build.sh
+++ poly_eval/scripts/build.sh
@@ -4,6 +4,6 @@ cd "$(dirname "$0")" || exit 1
 
 mkdir -p ../build
 
-zokrates compile -i ../poly_eval.zok -s ../build/abi.json -o ../build/out 
+zokrates compile -i ../poly_ec_eval.zok -s ../build/abi.json -o ../build/out 
 zokrates setup -i ../build/out --proving-key-path ../build/proving.key --verification-key-path ../build/verification.key
 zokrates export-verifier -i ../build/verification.key -o ../build/verifier.sol
