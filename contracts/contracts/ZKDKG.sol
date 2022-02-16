c86905305282dacfc0f23f95024d9896f80f2c18
Michael Sober <michael.sober@tuhh.de>
2022-02-16 13:20:27 +0100

    Register with public key

--- contracts/contracts/ZKDKG.sol
+++ contracts/contracts/ZKDKG.sol
@@ -7,13 +7,19 @@ import "./KeyVerifier.sol";
 contract ZKDKG {
     uint256 public constant MIN_STAKE = 0 ether;
 
-    mapping(address => uint256) private indices;
+    struct Participant {
+        uint256 index;
+        uint256[2] publicKey;
+    }
+
+    mapping(address => Participant) public participants;
     address[] private addresses;
+
     mapping(address => bytes32) public commitmentHashes;
     mapping(address => bytes32) private shareHashes;
     uint256[2][] private firstCoefficients;
 
-    uint256[2] private publicKey;
+    uint256[2] private masterPublicKey;
 
     ShareVerifier private shareVerifier;
     KeyVerifier private keyVerifier;
@@ -25,16 +31,27 @@ contract ZKDKG {
         keyVerifier = KeyVerifier(_keyVerifier);
     }
 
-    function register() public payable {
+    function register(uint256[2] memory publicKey) public payable {
         require(msg.value == MIN_STAKE, "value too low");
-        indices[msg.sender] = addresses.length;
+        require(participants[msg.sender].index == 0, "already registered");
         addresses.push(msg.sender);
+        participants[msg.sender] = Participant(addresses.length, publicKey);
     }
 
     function broadcastShares(
         uint256[2][] memory commitments,
         uint256[] memory shares
     ) external {
+        require(shares.length == addresses.length, "invalid number of shares");
+        require(participants[msg.sender].index != 0, "not registered");
+
+        uint256 threshold = (addresses.length + 1) / 2;
+
+        require(
+            commitments.length == threshold,
+            "invalid number of commitments"
+        );
+
         firstCoefficients.push(commitments[0]);
         commitmentHashes[msg.sender] = keccak256(abi.encodePacked(commitments));
         shareHashes[msg.sender] = keccak256(abi.encodePacked(shares)); // TODO: Store in merkle tree
@@ -49,7 +66,7 @@ contract ZKDKG {
         uint256[5] memory input = [
             hash[0],
             hash[1],
-            indices[dealer] + 1,
+            participants[dealer].index,
             share,
             1
         ];
@@ -71,7 +88,7 @@ contract ZKDKG {
             _publicKey[1]
         ];
         require(keyVerifier.verifyTx(proof, input), "invalid proof");
-        publicKey = _publicKey;
+        masterPublicKey = _publicKey;
     }
 
     function hashToUint128(bytes32 _hash)
