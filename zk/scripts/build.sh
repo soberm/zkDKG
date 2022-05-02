2e24a46c407237095371040426852891efc4c333
Max Kobelt <max.kobelt@tuhh.de>
2022-05-02 12:44:42 +0200

    Check if commits are valid points

--- zk/scripts/build.sh
+++ zk/scripts/build.sh
@@ -2,10 +2,17 @@
 
 cd "$(dirname "$0")" || exit 1
 
-build=../build/${1%.*}
+sourceName=${1%.*}
+build=../build/$sourceName
 
 mkdir -p $build
 
-zokrates compile -i ../"$@" -s $build/abi.json -o $build/out 
+compileFlags=""
+if [[ $sourceName == "poly_eval" ]]; then
+    # Required for checking if the commits are valid points
+    compileFlags="--isolate-branches"
+fi
+
+zokrates compile -i ../"$@" -s $build/abi.json -o $build/out $compileFlags
 zokrates setup -i $build/out --proving-key-path $build/proving.key --verification-key-path $build/verification.key
 zokrates export-verifier -i $build/verification.key -o $build/verifier.sol
