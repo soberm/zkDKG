4c79f121548d8c0f433a86d43b4349c86b76e69e
Max Kobelt <max.kobelt@tuhh.de>
2022-06-20 15:06:33 +0200

    Fix visibility of DH function

--- dkg/pkg/dkg/dh.go
+++ dkg/pkg/dkg/dh.go
@@ -5,7 +5,7 @@ import (
 	"go.dedis.ch/kyber/v3/suites"
 )
 
-func dhExchange(suite suites.Suite, ownPrivate kyber.Scalar, remotePublic kyber.Point) kyber.Point {
+func DhExchange(suite suites.Suite, ownPrivate kyber.Scalar, remotePublic kyber.Point) kyber.Point {
 	sk := suite.Point()
 	sk.Mul(ownPrivate, remotePublic)
 	return sk
