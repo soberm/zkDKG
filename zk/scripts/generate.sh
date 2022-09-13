2f65e80fbd6881e09ddff08be9b9a522f70b2480
Max Kobelt <max.kobelt@tuhh.de>
2022-09-13 15:12:07 +0200

    Remove Zokrates scripts

--- zk/scripts/generate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")" || exit 1
-
-build=../build/${1%.*}
-
-zokrates compute-witness -i $build/out -s $build/abi.json -a "${@: 2}" -o $build/witness
-zokrates generate-proof -i $build/out --proof-path $build/proof.json -p $build/proving.key -w $build/witness
--- zk/scripts/verify.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")" || exit 1
-
-build=../build/${1%.*}
-
-zokrates verify -j $build/proof.json -v $build/verification.key
